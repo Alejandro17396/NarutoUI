e03060f38a13ee77577ad54d3e527e5be1f82602
Alejandro <alex17396@hotmail.com>
2022-05-01 02:03:25 +0200

    version 4 en host

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/"naruto-app-ui'));
+app.use(express.static(__dirname + '/dist/naruto-app-ui'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/"naruto-app-ui/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/naruto-app-ui/index.html'));
 });
 
 // default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8085);
